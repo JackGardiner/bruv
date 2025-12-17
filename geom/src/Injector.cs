9c6d189f2d846fb5d371f06e6d8932dc8cd56604
Jack Gardiner <jack7gardiner@gmail.com>
2025-12-18 07:40:40 +1300

    ports and supports

--- geom/src/Injector.cs
+++ geom/src/Injector.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using br;
 using Leap71.ShapeKernel;
 using PicoGK;
 using static System.MathF;
@@ -36,6 +38,7 @@ public class Injector
         public float fLOxPostWT { get; set; }
         public float fInjectorPlateThickness { get; set; }
         public float fFuelAnnulusOR { get; set; }
+        public float fInnerOringPadW {get; set; }
         public float fInnerOringW {get; set; }
         public float fInnerOringMR {get; set; }
         public float fInnerOringDepth {get; set; }
@@ -101,26 +104,28 @@ public class Injector
         // constructor: initialise what we already can
         public InterfacesConfig(SimConfig s)
         {
-            fInnerOringMR = s.inj.fInnerOringMR;
-            fInnerOringIR = s.inj.fInnerOringMR - s.inj.fInnerOringW/2f;
-            fInnerOringOR = s.inj.fInnerOringMR + s.inj.fInnerOringW/2f;
-            fInnerOringDepth = s.inj.fInnerOringDepth;
+            fCoolingChannelWidth = 3f; // should be a little larger than the chamber channel width
 
-            fOuterOringMR = s.inj.fOuterOringMR;
-            fOuterOringIR = s.inj.fOuterOringMR - s.inj.fOuterOringW/2f;
-            fOuterOringOR = s.inj.fOuterOringMR + s.inj.fOuterOringW/2f;
+            //calculate inner o-ring position
+            fInnerOringIR = s.eng.fChamberRadius + 2f; // placeholder clearance for now - need calcs
+            fInnerOringOR = fInnerOringIR + s.inj.fInnerOringW;
+            fInnerOringDepth = s.inj.fInnerOringDepth; // should probs not be in two place
+
+            float fOuterAnnulusBoundingRad = s.inj.aElementRadii[^1] + s.inj.fFuelAnnulusOR;
+            float fFilmCoolingBoundingRad = s.inj.aFilmHoleRadii[^1] + s.inj.aFilmHoleRadii[^1];
+            fCoolingChannelIR = fInnerOringOR + 2f; // yet another placeholder offset
+            fCoolingChannelMR = fCoolingChannelIR + 0.5f*fCoolingChannelWidth;
+            fCoolingChannelOR = fCoolingChannelIR + fCoolingChannelWidth;
+
+            fOuterOringIR = fCoolingChannelOR + 2f; // placeholder offset
+            fOuterOringOR = fOuterOringIR + s.inj.fOuterOringW;
             fOuterOringDepth = s.inj.fOuterOringDepth;
-
-            fCoolingChannelMR = 55f; //TODO fix magic number
-            fCoolingChannelWidth = 3f; //TODO as above
-            fCoolingChannelIR = fCoolingChannelMR - fCoolingChannelWidth/2f;
-            fCoolingChannelOR = fCoolingChannelMR + fCoolingChannelWidth/2f;
 
             fBoltMR = 80f; //TODO as above
         }
     }
 
-    public static void Task()
+    public static Voxels maker()
     {
         // placeholder task fn for just the injector
         Injector oCoaxShearInjector = new Injector();
@@ -130,7 +135,7 @@ public class Injector
                             fTransparency: 0.9f,
                             fMetallic: 0.4f,
                             fRoughness: 0.3f);
-        Sh.PreviewFrame(new LocalFrame(new Vector3(0f, 56.5f, 0f)), 10f);
+        return voxCoaxShearInjector;
     }
 
     public Injector()
@@ -161,7 +166,6 @@ public class Injector
         return aInjectorLocations;
     }
 
-
     public Voxels voxInjectorPlate(List<Vector3> aInjElements, List<Vector3> aFilmElements, float fFilmCoolingHoleRadius,
                                     float fAnnulusRadius, float fPlateThickness, float fOuterRadius)
     {
@@ -182,6 +186,10 @@ public class Injector
             BaseLens oHole = new BaseLens(new LocalFrame(aFilmHole), fPlateThickness, 0, fFilmCoolingHoleRadius);
             voxPlate = voxPlate - oHole.voxConstruct();
         }
+
+        // remove hole for ignitor (//TODO fix placeholder)
+        BaseLens oASIHole = new BaseLens(new LocalFrame(), fPlateThickness, 0, 4.0f);
+        voxPlate = voxPlate - oASIHole.voxConstruct();
         return voxPlate;
     }
 
@@ -193,44 +201,128 @@ public class Injector
 
     public Voxels voxASIPassThrough()
     {
-        BSPPPort ASIPort = new BSPPPort("1/4in", 6.35f); // 1/4in OD for SS insert?
+        GPort ASIPort = new GPort("1/4in", 6.35f); // 1/4in OD for SS insert?
 
-        return new Voxels(); //TODO
+        Voxels voxPort = ASIPort.voxConstruct(new LocalFrame(new Vector3(0,0,50)));
+
+        return voxPort; //TODO
+    }
+
+    public List<Voxels> voxPorts(ConeRoof oRoof)
+    {
+        Voxels voxLOXCrop = oRoof.voxUpperCrop();
+        Voxels voxIPACrop = oRoof.voxLowerCrop();
+
+        // determine vertical height of ports:  for now use placeholder
+        float fPortsHeight = 50f;
+        // determine spacing:  ASI in middle, LOX inlet and 3x PT spaced evenly
+        float fPortRadialDistance = 0.5f*s.eng.fChamberRadius;
+
+        // create vectors
+        LocalFrame aLOXInlet = new LocalFrame(new Vector3(-fPortRadialDistance, 0, fPortsHeight));
+        LocalFrame aLOXPT =  new LocalFrame(new Vector3(fPortRadialDistance, 0, fPortsHeight));
+        LocalFrame aChamberPT =  new LocalFrame(new Vector3(0, -fPortRadialDistance, fPortsHeight));
+        LocalFrame aIPAPT =  new LocalFrame(new Vector3(0, fPortRadialDistance, fPortsHeight));
+
+        GPort oLOXInlet = new GPort("1/2in", 14f);
+        BasePipe oLOXInletPipe = new BasePipe(aLOXInlet, -fPortsHeight, 0f, 14f/2);
+        Voxels voxLOXInletFluid = oLOXInlet.voxConstruct(aLOXInlet) + oLOXInletPipe.voxConstruct();
+        Voxels voxLOXInletWall = voxLOXInletFluid.voxOffset(2f);
+        voxLOXInletFluid = voxLOXInletFluid - voxLOXCrop;
+        voxLOXInletWall = voxLOXInletWall - voxLOXCrop;
+
+        GPort oLOXPT = new GPort("1/4in", 7.5f);
+        BasePipe oLOXPTPipe = new BasePipe(aLOXPT, -fPortsHeight, 0f, 7.5f/2);
+        Voxels voxLOXPTFluid = oLOXPT.voxConstruct(aLOXPT) + oLOXPTPipe.voxConstruct();
+        Voxels voxLOXPTWall = voxLOXPTFluid.voxOffset(2f);
+        voxLOXPTFluid = voxLOXPTFluid - voxLOXCrop;
+        voxLOXPTWall = voxLOXPTWall - voxLOXCrop;
+
+        GPort oIPAPT = new GPort("1/4in", 7.5f);
+        BasePipe oIPAPTPipe = new BasePipe(aIPAPT, -fPortsHeight, 0f, 7.5f/2);
+        Voxels voxIPAPTFluid = oIPAPT.voxConstruct(aIPAPT) + oIPAPTPipe.voxConstruct();
+        Voxels voxIPAPTWall = voxIPAPTFluid.voxOffset(2f);
+        voxIPAPTFluid = voxIPAPTFluid - voxIPACrop;
+        voxIPAPTWall = voxIPAPTWall - voxIPACrop;
+
+        GPort oChamberPT = new GPort("1/8in", 4.5f);
+        BasePipe oChamberPTPipe = new BasePipe(aChamberPT, -fPortsHeight, 0f, 7.5f/2);
+        Voxels voxChamberPTFluid = oChamberPT.voxConstruct(aChamberPT) + oChamberPTPipe.voxConstruct();
+        Voxels voxChamberPTWall = voxChamberPTFluid.voxOffset(2f);
+
+        Sh.PreviewFrame(aLOXInlet, 5f);
+        Sh.PreviewFrame(aLOXPT, 5f);
+        Sh.PreviewFrame(aChamberPT, 5f);
+        Sh.PreviewFrame(aIPAPT, 5f);
+
+        Voxels voxFluids = voxLOXInletFluid + voxLOXPTFluid + voxIPAPTFluid + voxChamberPTFluid;
+        Voxels voxWalls = voxLOXInletWall + voxLOXPTWall + voxIPAPTWall + voxChamberPTWall - voxCropBoxZ(fPortsHeight) - voxCropBoxZ(0f, 100f, -10f);
+
+        // return list of voxels:  {wall voxels, fluid voxels}
+        return new List<Voxels> {voxWalls, voxFluids};
+    }
+
+    public Voxels voxPrisonBarSupports(ConeRoof oRoof, float fZ, float fMiddleRadius, float fRadius, int iNumBars)
+    {
+        // bar type supports for the manifold dividing wall
+        // cylinder for now, make better later (//TODO change bar supports to arc segments)
+        float fDeltaPhi = 2*PI / iNumBars;
+        Voxels voxBars = new Voxels();
+        for (int i=0; i<iNumBars; i++)
+        {
+            float fPhi = i*fDeltaPhi;
+            float fX = fMiddleRadius*Cos(fPhi);
+            float fY = fMiddleRadius*Sin(fPhi);
+            Vector3 oBarLocation = new Vector3(fX, fY, fZ);
+            BasePipe oBar = new BasePipe(new LocalFrame(oBarLocation), 100f, 0f, fRadius);
+            voxBars += oBar.voxConstruct();
+        }
+        voxBars.BoolIntersect(oRoof.voxLowerCrop());
+        return voxBars;
     }
 
     public class ConeRoof
     {
-        // inputs
+        // inputsw
         private readonly InjectorConfig Inj;
         private readonly SlmConfig Slm;
         private readonly InterfacesConfig Ifc;
         private readonly List<Vector3> m_aPointsList;
+        private readonly Vector3 m_aASILocation;
         private readonly float m_fRoofThickness;
 
         // outputs
 
         // constructor
         public ConeRoof(InjectorConfig inj, SlmConfig slm, InterfacesConfig ifc,
-                        List<Vector3> aPointsList, float fRoofThickness)
+                        List<Vector3> aPointsList, Vector3 aASILocation, float fRoofThickness)
         {
             Inj = inj;
             Slm = slm;
             Ifc = ifc;
             m_aPointsList = aPointsList;
+            m_aASILocation = aASILocation;
             m_fRoofThickness = fRoofThickness;
         }
 
         // public query methods
-        // public Voxels voxUpperCrop()
-        // {
-        //     BaseLens oCrop = new BaseLens(new LocalFrame(), 0f, 0f,m_fOuterRadius);
-        //     oCrop.SetHeight(fConstantSurfaceModulation(0f), )
-        //     return new Voxels();
-        // }
-
-        public Voxels voxLowerCropVolume()
+        public Voxels voxUpperCrop()
         {
-            return new Voxels();
+            BaseLens oCrop = new BaseLens(new LocalFrame(), 1f, 0f, Ifc.fCoolingChannelOR);
+            oCrop.SetHeight(new SurfaceModulation(0f), new SurfaceModulation(fGetAtticLowerHeight));
+            return oCrop.voxConstruct();
+        }
+
+        public Voxels voxLowerCrop()
+        {
+            BaseLens oCrop = new BaseLens(new LocalFrame(), 1f, 0f, Ifc.fCoolingChannelOR);
+            oCrop.SetHeight(new SurfaceModulation(0f), new SurfaceModulation(fGetConeRoofLowerHeight));
+            return oCrop.voxConstruct();
+        }
+
+        public float fOuterBoundingRadius()
+        {
+            return 0f;
         }
 
         // vox builder
@@ -256,32 +348,40 @@ public class Injector
 
                 voxRoof = voxRoof + oInjectorWall.voxConstruct();
                 voxRoof = voxRoof - oInjectorFluid.voxConstruct();
-                voxRoof = voxRoof - voxLOxCrop;
             }
+            voxRoof = voxRoof - voxLOxCrop; // crop away stuff
 
+            // add top roof (attic)
+            BaseLens oAttic = new BaseLens(new LocalFrame(), m_fRoofThickness, 0f, Ifc.fCoolingChannelOR);
+            oAttic.SetHeight(new SurfaceModulation(fGetAtticLowerHeight), new SurfaceModulation(fGetAtticUpperHeight));
+            Voxels voxAttic = new Voxels(oAttic.voxConstruct());
 
-            return voxRoof;
+            // create augmented spark igniter through-port
+            BasePipe oASIWall = new BasePipe(new LocalFrame(m_aASILocation), 50f, 6.35f, 10.35f);
+            BasePipe oASIFluid = new BasePipe(new LocalFrame(m_aASILocation), 50f, 0, 6.35f);
+            Voxels voxOut = voxRoof + voxAttic + oASIWall.voxConstruct() - oASIFluid.voxConstruct();
+
+            return voxOut;
         }
 
         // private helpers
-        private float fConstantSurfaceModulation(float fHeight)
-        {
-            return fHeight;
-        }
         private float fGetConeRoofSurfaceModulation(float fPhi, float fLengthRatio)
         {
             float fRadius = fLengthRatio * Ifc.fCoolingChannelOR;
             float fZVal = (Ifc.fCoolingChannelOR - fRadius)*Tan(Slm.fPrintAngle) + Inj.fInjectorPlateThickness;  // outer wall case
             // calculate vertical offset required (for now use cone mid-plane)
             float fConeOffset = Inj.fLOxPostLength - (Inj.fLOxPostFluidRad+Inj.fLOxPostWT)*Tan(Slm.fPrintAngle);
-            foreach (Vector3 aPoint in m_aPointsList)
+            List<Vector3> aAllPoints = new List<Vector3>(m_aPointsList) { m_aASILocation };
+
+            foreach (Vector3 aPoint in aAllPoints)
             {
                 float fPointPhi = Atan2(aPoint.Y, aPoint.X);
                 float fPointRad = Sqrt((aPoint.X*aPoint.X)+(aPoint.Y*aPoint.Y));
 
                 float fTrialZ = Sqrt((fRadius*fRadius)+(fPointRad*fPointRad)-(2*fRadius*fPointRad*Cos(fPhi-fPointPhi)))*Tan(Slm.fPrintAngle) + fConeOffset;
-
-                if (fTrialZ < fZVal){fZVal = fTrialZ;}
+                
+                if (float.IsNaN(fZVal)) {fZVal = fTrialZ;}
+                else if (fTrialZ < fZVal){fZVal = fTrialZ;}
             }            
         
             return fZVal;
@@ -302,9 +402,25 @@ public class Injector
             return fGetConeRoofSurfaceModulation(fPhi, fLengthRatio) + Cos(Slm.fPrintAngle)*m_fRoofThickness*2f;
         }
 
+        private float fGetAtticSurfaceModulation(float fPhi, float fLengthRatio)
+        {
+            float fRadius = fLengthRatio * Ifc.fCoolingChannelOR;
+            float fInnerWallZ = Sqrt(fRadius*fRadius)*Tan(Slm.fPrintAngle) + 3f; // fix
+            float fOuterWallZ = (Ifc.fCoolingChannelOR - fRadius)*Tan(Slm.fPrintAngle) + Inj.fInjectorPlateThickness;
+            return Min(fInnerWallZ, fOuterWallZ);
+        }
+        private float fGetAtticLowerHeight(float fPhi, float fLengthRatio)
+        {
+            return fGetAtticSurfaceModulation(fPhi, fLengthRatio);
+        }
+        private float fGetAtticUpperHeight(float fPhi, float fLengthRatio)
+        {
+            return fGetAtticSurfaceModulation(fPhi, fLengthRatio) + Cos(Slm.fPrintAngle)*m_fRoofThickness;
+        }
+
     }
 
-    public class BSPPPort
+    public class GPort
     {
         // inputs
         private readonly string m_sSize;
@@ -316,7 +432,7 @@ public class Injector
         private readonly float m_fBoreDepthTotal;
 
         // Constructor
-        public BSPPPort(string sSize, float fDownstreamDiameter)
+        public GPort(string sSize, float fDownstreamDiameter)
         {
             m_sSize = sSize;
             m_fDownstreamDiameter = fDownstreamDiameter;
@@ -375,19 +491,24 @@ public class Injector
             
             switch (sSize)
             {
+                case "1/8in":
+                    fBoreDiam = 9.73f;
+                    fRequiredFullThreadLength = 10.5f;
+                    fFaceDiam = 17.2f;
+                    break;
                 case "1/4in":
                     fBoreDiam = 13.3f;
-                    fRequiredFullThreadLength = 12.4f;
+                    fRequiredFullThreadLength = 15.5f;
                     fFaceDiam = 20.7f;
                     break;
                 case "1/2in":
                     fBoreDiam = 21.1f;
-                    fRequiredFullThreadLength = 14.5f;
+                    fRequiredFullThreadLength = 19f;
                     fFaceDiam = 34.0f;
                     break;
                 case "3/4in":
                     fBoreDiam = 26.6f;
-                    fRequiredFullThreadLength = 16.5f;
+                    fRequiredFullThreadLength = 20.5f;
                     fFaceDiam = 40.0f;
                     break;
                 default:
@@ -400,33 +521,43 @@ public class Injector
         }
     }
 
+    public Voxels voxCropBoxZ(float fZ, float fR=100, float fH=100)
+    {
+        BaseCylinder oCropCyl = new BaseCylinder(new LocalFrame(new Vector3(0f, 0f, fZ)), fH, fR);
+        return oCropCyl.voxConstruct();
+    }
+
     public Voxels voxConstruct()
     {
-
         List<Vector3> aInjectorLocations = InjectorPattern(s.inj.aElementCount, s.inj.aElementRadii, s.inj.aElementClocking);
         List<Vector3> aFilmCoolingLocations = InjectorPattern(s.inj.aFilmHoleCount, s.inj.aFilmHoleRadii, s.inj.aFilmHoleClocking);
 
+        Sh.PreviewCylinderWireframe(new BaseCylinder(new LocalFrame(), -50, i.fCoolingChannelIR), Cp.clrBlack);
+        Sh.PreviewCylinderWireframe(new BaseCylinder(new LocalFrame(), -50, i.fCoolingChannelOR), Cp.clrBlack);
+        Sh.PreviewCylinderWireframe(new BaseCylinder(new LocalFrame(), -50, s.eng.fChamberRadius), Cp.clrBlack);
 
-        ConeRoof oDividingWall = new ConeRoof(s.inj, s.slm, i, aInjectorLocations, 2f);
+
+
+        Sh.PreviewPointCloud(aInjectorLocations, 1f, Cp.clrGreen);
+        Sh.PreviewPointCloud(aFilmCoolingLocations, 1f, Cp.clrLavender);
+
+        ConeRoof oDividingWall = new ConeRoof(s.inj, s.slm, i, aInjectorLocations, new Vector3(), 2f);
         Voxels voxDividingWall = oDividingWall.voxConstruct();
 
         Voxels voxFacePlate = new Voxels(voxInjectorPlate(aInjectorLocations, aFilmCoolingLocations, 0.5f,
                                                             s.inj.fFuelAnnulusOR, s.inj.fInjectorPlateThickness, i.fBoltMR));
         Voxels voxInnerOringGroove = new Voxels(voxGroovyBaby(new LocalFrame(), i.fInnerOringDepth, i.fInnerOringIR, i.fInnerOringOR));
         Voxels voxOuterOringGroove = new Voxels(voxGroovyBaby(new LocalFrame(), i.fOuterOringDepth, i.fOuterOringIR, i.fOuterOringOR));
+        Voxels voxRegenSlot = new Voxels(voxGroovyBaby(new LocalFrame(), s.inj.fInjectorPlateThickness, i.fCoolingChannelIR, i.fCoolingChannelOR));
 
-        Voxels voxRegenInletGroove = new Voxels(voxGroovyBaby(new LocalFrame(), s.inj.fInjectorPlateThickness, 
-                                                i.fCoolingChannelIR, i.fCoolingChannelOR));
+        Voxels voxGrooves = voxInnerOringGroove + voxOuterOringGroove + voxRegenSlot;
 
-        Voxels voxGrooves = voxInnerOringGroove + voxOuterOringGroove + voxRegenInletGroove;
+        Voxels voxInletsWalls = voxPorts(oDividingWall)[0];
+        Voxels voxInletsFluids = voxPorts(oDividingWall)[1];
 
-        BSPPPort oASIPort = new BSPPPort("1/4in", 7f);
-        BSPPPort oLOXPort = new BSPPPort("1/2in", 15f);
-        BSPPPort oPTPort  = new BSPPPort("1/4in", 5f);
+        Voxels voxBars = voxPrisonBarSupports(oDividingWall, 2, 40, 1, 40);
 
-        // Voxels voxPorts = oASIPort.voxConstruct();
-
-        Voxels voxOutput = voxDividingWall + voxFacePlate - voxGrooves;// + oASIPort.voxConstruct(new LocalFrame(new Vector3(0f, 0f, 0f)));;
+        Voxels voxOutput = voxDividingWall + voxFacePlate - voxGrooves + voxInletsWalls - voxInletsFluids + voxBars;
         BBox3 bounds = voxOutput.oCalculateBoundingBox();
         if (bounds.vecMin.X < 0f) {
             bounds.vecMin.X = 0f;
@@ -435,5 +566,4 @@ public class Injector
         }
         return voxOutput;
     }       
-    
 }
